00ff83536cacc2edea89361751d5f81e6239d4d0
shaxverdiev <shaxverdievv@gmail.com>
2022-10-07 04:22:59 +0300

    cors

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require("./db/db");
 const express = require("express");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-const errorMiddleware = require('./middlewares/error.middlerware')
+const errorMiddleware = require("./middlewares/error.middlerware");
 
 const router = require("./routes/index.routes");
 
@@ -12,7 +12,11 @@ const PORT = process.env.PORT;
 const app = express();
 
 app.use(require("morgan")("dev"));
-app.use(require("cors")());
+app.use(
+  require("cors")({
+    origin: "*",
+  })
+);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
